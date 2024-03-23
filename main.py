ec220f89eed22b0694e4cc49a66e4f64aaf1622b
Prajit Adhikari <khalidchaudhari@yahoo.com>
2024-03-23 18:38:32 +0545

    DMO certificate logic

--- main.py
+++ main.py
@@ -198,12 +198,8 @@ def main():
         certificate_type = st.selectbox("Select certificate type", ["DMO", "PMO", "NMO"])
         if st.button("Generate"):
             if certificate_type == "DMO":
-                if student_name in dict_dmo_2024:
-                    image_bytes = generate_dmo2024_certificate(student_name, "COMIC.TTF",
-                                                   "./2024_certificates/certificate for DMO.png")
-                else:
-                    st.error(f"{student_name} is not in the list of students.")
-                    return None
+                image_bytes = generate_dmo2024_certificate(student_name, "COMIC.TTF",
+                                                "./2024_certificates/certificate for DMO.png")
             elif certificate_type == "PMO":
                 if student_name in dict_pmo_2024:
                     image_bytes = generate_pmo_certificate(student_name, "COMIC.TTF",
