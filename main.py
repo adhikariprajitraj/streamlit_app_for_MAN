f8c9260ca522bcf64f7829919d18e9bc778f3fbf
Prajit Adhikari <khalidchaudhari@yahoo.com>
2024-08-22 15:23:45 +0545

    positioning of name

--- main.py
+++ main.py
@@ -178,8 +178,11 @@ def generate_tst2024_certificate(name, font_path, certificate_path):
         draw.text((650, 710), symbol_no, font=font2, fill=(0, 0, 0))
         # if name is longer than 20 characters, reduce the font size
         if len(name) > 20:
-            font1 = ImageFont.truetype(font_path, 120)
-        draw.text((690, 1150), name, font=font1, fill=(0, 0, 0))
+            font1 = ImageFont.truetype(font_path, 130)
+            # change x coordinate to 650
+            draw.text((650, 1150), name, font=font1, fill=(0, 0, 0))
+        else:
+            draw.text((690, 1150), name, font=font1, fill=(0, 0, 0))
         image_bytes = io.BytesIO()
         image.save(image_bytes, 'PNG')
         image_bytes.seek(0)
